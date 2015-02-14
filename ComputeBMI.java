9b93c5f97285401b90ea90caa4117b9915f605c1
Christopher <icemancad@gmail.com>
2015-02-14 14:11:59 -0600

    Assignment 2

---
+++ ComputeBMI.java
@@ -0,0 +1,15 @@
+import javax.swing.JOptionPane;
+
+public class ComputeBMI {
+	public static void main(String[] args) {
+		String input = JOptionPane
+				.showInputDialog("Enter your weight in pounds");
+		double weight = Double.parseDouble(input);
+		input = JOptionPane.showInputDialog("Enter your height in inches");
+		double height = Double.parseDouble(input);
+		final double LBTOKG = .45359237;
+		final double INTOM = .0254;
+		double bmi = (weight * LBTOKG) / (height * INTOM);
+		JOptionPane.showMessageDialog(null, "Your Body Mass Index is " + bmi);
+	}
+}
---
+++ MilesPerGallon.java
@@ -0,0 +1,13 @@
+import javax.swing.JOptionPane;
+
+public class MilesPerGallon {
+	public static void main(String[] args) {
+		String input = JOptionPane
+				.showInputDialog("Enter the total miles driven");
+		double miles = Double.parseDouble(input);
+		input = JOptionPane.showInputDialog("Enter the number of gallons used");
+		double gallons = Double.parseDouble(input);
+		double mpg = (miles / gallons);
+		JOptionPane.showMessageDialog(null, "Your Miles-per-Gallon is " + mpg);
+	}
+}
